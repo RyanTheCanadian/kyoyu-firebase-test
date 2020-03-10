404a17fd55c308e0cc7ffd71854091fe593d29cc
RyanTheCanadian <ryanyap555@gmail.com>
2020-03-10 23:46:26 +0800

    3 files modified

--- index.js
+++ index.js
@@ -30,26 +30,3 @@ var firebaseConfig = {
 // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-
-
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       name: 'React'
-//     };
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <Hello name={this.state.name} />
-//         <p>
-//           Start editing to see some magic happen :)
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
-// render(<App />, document.getElementById('root'));
--- signup.js
+++ signup.js
@@ -1,48 +1,54 @@
-// import React, { Component } from 'react';
-// import { render } from 'react-dom';
-// import Hello from './Hello';
-// import './style.css';
-// import './main.css';
-// import './index.css';
+(function(){
 
-// Firebase App (the core Firebase SDK) is always required
-// and must be listed first
-import * as firebase from "firebase/app";
-
-// Add the Firebase products that you want to use
-import "firebase/auth";
-import "firebase/firestore";
-
-import * as firebaseui from "firebaseui";
-
-// Add Firebase project configuration object here
-var firebaseConfig = {
-    apiKey: "AIzaSyBlnT8Z2kPBuHpHQagq7511dAQxwWc1SAg",
-    authDomain: "kyoyu-carpooling-system.firebaseapp.com",
-    databaseURL: "https://kyoyu-carpooling-system.firebaseio.com",
-    projectId: "kyoyu-carpooling-system",
-    storageBucket: "kyoyu-carpooling-system.appspot.com",
-    messagingSenderId: "1075899178529",
-    appId: "1:1075899178529:web:46efdd5e82d89dfb098d60",
-    measurementId: "G-YFTW3PRSS6"
+  //Initialize Firebase
+  const firebaseConfig = {
+  apiKey: "AIzaSyBlnT8Z2kPBuHpHQagq7511dAQxwWc1SAg",
+  authDomain: "kyoyu-carpooling-system.firebaseapp.com",
+  databaseURL: "https://kyoyu-carpooling-system.firebaseio.com",
+  projectId: "kyoyu-carpooling-system",
+  storageBucket: "kyoyu-carpooling-system.appspot.com",
+  messagingSenderId: "1075899178529",
+  appId: "1:1075899178529:web:46efdd5e82d89dfb098d60",
+  measurementId: "G-YFTW3PRSS6"
 };
+firebase.initializeApp(firebaseConfig);
 
-// Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
+//Get elements
+const txtEmail = document.getElementById('txtEmail');
+const txtPassword = document.getElementById('txtPassword');
+const btnLogin = document.getElementById('txtLogin');
+const btnSignup = document.getElementById('txtSignUp');
+const btnLogout = document.getElementById('txtLogout');
 
-// FirebaseUI config
-const uiConfig = {
-  credentialHelper: firebaseui.auth.CredentialHelper.NONE,
-  signInOptions: [
-    // Email / Password Provider.
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
-  ],
-  callbacks: {
-    signInSuccessWithAuthResult: function(authResult, redirectUrl){
-      // Handle sign-in.
-      // Return false to avoid redirect.
-      return false;
-    }
+//Add login event
+btnLogin.addEventListener('click', e => {
+  //Get email and psssword
+  const email = txtEmail.value;
+  const pass = txtPassword.value;
+  const auth = firebase.auth();
+  //Sign In 
+  const promise = auth.signInWithEmailAndPassword(email,pass);
+  promise.catch(e => console.log(e.message));
+})
+
+//Add signup event 
+btnSignUp.addEventistener('click', e => {
+  //Get email and psssword
+  //TODO: Check that its a real email
+  const email = txtEmail.value;
+  const pass = txtPassword.value;
+  const auth = firebase.auth();
+  //Sign In 
+  const promise = auth.createUserWithEmailAndPassword(email,pass);
+  promise.catch(e => console.log(e.message));
+});
+
+//Add a realtime listener 
+firebase.auth().onAuthStateChanged(firebaseUser => {
+  if(firebaseUser) {
+    console.log(firebaseUser);
+  }else{
+    console.log('not logged in');
   }
-};
+})
+}());
