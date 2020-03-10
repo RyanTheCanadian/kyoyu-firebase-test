51973a8d65c82f9924bbba1e7c4f00f288038d03
RyanTheCanadian <ryanyap555@gmail.com>
2020-03-10 19:56:30 +0800

    Firebase js file

---
+++ src/signup.js
@@ -0,0 +1,31 @@
+// Import stylesheets
+
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from "firebase/app";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
+import * as firebaseui from 'firebaseui';
+
+// Add Firebase project configuration object here
+// var firebaseConfig = {};
+
+// firebase.initializeApp(firebaseConfig);
+
+// FirebaseUI config
+const uiConfig = {
+  credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+  signInOptions: [
+    // Email / Password Provider.
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+  callbacks: {
+    signInSuccessWithAuthResult: function(authResult, redirectUrl){
+      // Handle sign-in.
+      // Return false to avoid redirect.
+      return false;
+    }
+  }
+};
